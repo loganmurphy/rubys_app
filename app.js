7c6860d6184c8b99547df80bb00a631142ca8afc
loganmurphy <loganmurphy1984@gmail.com>
2017-11-09 10:43:27 -0600

    fixed bug on app.js

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ app.set('view engine', 'hbs');
 app.get('/', function(req, res){
   var context = {title: 'Whack-a-Cat!'};
   res.render('index.hbs', context);
-})
+});
 
 var PORT = process.env.PORT || 8080;
 app.listen(PORT, function () {
-  console.log('Your app is up on port 8080. Let''s Whack-a-Cat!');
+  console.log('Your app is up on port 8080. Let\'s Whack-a-Cat!');
 });
